7b5262f7264786337a098d45f2bd187e8da95407
bvkeersop <bart.van.keersop@gmail.com>
2018-04-24 12:24:16 +0200

    added the right getter and setter

--- src/main/java/entities/Vehicle.java
+++ src/main/java/entities/Vehicle.java
@@ -82,4 +82,12 @@ public class Vehicle implements Serializable {
 	public void setTranslocations(List<Translocation> translocations) {
 		this.translocations = translocations;
 	}
+	
+	public String getHardwareSn() {
+		return hardwareSn;
+	}
+
+	public void setHardwareSn(String hardwareSn) {
+		this.hardwareSn = hardwareSn;
+	}
 }
