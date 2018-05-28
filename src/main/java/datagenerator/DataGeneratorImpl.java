9b54d4a8f10ddfb4b462de711c70827d93377253
bvkeersop <bart.van.keersop@gmail.com>
2018-05-28 09:54:09 +0200

    Generate endpoint now always works

--- src/main/java/datagenerator/DataGeneratorImpl.java
+++ src/main/java/datagenerator/DataGeneratorImpl.java
@@ -8,6 +8,7 @@ import entities.Vehicle;
 import exceptions.CategoryException;
 import exceptions.TranslocationException;
 import exceptions.VehicleException;
+import io.sentry.Sentry;
 import services.CategoryService;
 
 import javax.ejb.Stateless;
@@ -15,6 +16,7 @@ import javax.inject.Inject;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Stateless
 public class DataGeneratorImpl implements DataGenerator {
@@ -30,27 +32,61 @@ public class DataGeneratorImpl implements DataGenerator {
 
 	@Override
 	public void generateTestData() throws VehicleException, TranslocationException, CategoryException {
+		try {
 
-		List<Translocation> translocations = new ArrayList<>();
+			List<Translocation> translocations = new ArrayList<>();
 
-		LocalDateTime timestamp = LocalDateTime.now();
+			LocalDateTime timestamp = LocalDateTime.now();
 
-		Category category = new Category("TRUCK");
-		categoryService.createCategory(category);
+			Category category = getCategory();
+			String licensePlate = getUniqueLicensePlate();
 
-		Vehicle vehicle = new Vehicle(0, "LICENSPLATE", "MERCEDES", "VRACHTWAGEN", category, translocations, "HARDWARESN", "NL");
-		vehicleDao.createVehicle(vehicle);
+			Vehicle vehicle = new Vehicle(0, licensePlate, "MERCEDES", "VRACHTWAGEN", category, translocations, "HARDWARESN", "NL");
+			Vehicle vehicle2 = new Vehicle(0, licensePlate + "2", "FERRARI", "VRACHTWAGEN", category, translocations, "HARDWARESN", "FI");
+			vehicleDao.createVehicle(vehicle);
+			vehicleDao.createVehicle(vehicle2);
 
-		for (int i = 0; i <= 100; i++ ){
-			int minutesToAdd = 10;
+			for (int i = 0; i <= 100; i++) {
+				int minutesToAdd = 10;
 
-			if (i == 25 || i == 33 || i == 68 || i == 77){
-				minutesToAdd = 30;
+				if (i == 25 || i == 33 || i == 68 || i == 77) {
+					minutesToAdd = 30;
+				}
+
+				timestamp = timestamp.plusMinutes(minutesToAdd);
+				Translocation translocation = new Translocation(vehicle, i, i, timestamp, "FI", false);
+				Translocation translocation2 = new Translocation(vehicle2, i, i, timestamp, "FI", false);
+				translocationDao.createTranslocation(translocation);
+				translocationDao.createTranslocation(translocation2);
 			}
-
-			timestamp = timestamp.plusMinutes(minutesToAdd);
-			Translocation translocation = new Translocation(vehicle, i, i, timestamp, "FI", false);
-			translocationDao.createTranslocation(translocation);
+		}
+		catch(Exception e){
+			Sentry.capture(e.toString());
+			System.out.println(e.toString());
 		}
 	}
+
+	private String getUniqueLicensePlate() throws VehicleException {
+		Random rand = new Random();
+		String licensePlate = "LICENSPLATE";
+		while(vehicleDao.checkIfLicensePlateAlreadyExists(-1, licensePlate)){
+			licensePlate += Integer.toString(rand.nextInt(8) + 1);
+		}
+		return licensePlate;
+	}
+
+	private Category getCategory(){
+		Category category;
+
+		List<Category> categories = categoryService.getCategories();
+
+		if (categories == null || categories.size() == 0) {
+			category = new Category("TRUCK");
+		}
+		else {
+			category = categories.get(0);
+		}
+		return category;
+	}
+
 }
